3a26e8c433c3e18f243cc1c2023e8ef33cc2ffbd
Dostoevsky <kant.seemee1776@gmail.com>
2023-11-04 22:39:55 -0500

    Refactor CLI.

--- src/cli.rs
+++ src/cli.rs
@@ -52,121 +52,149 @@ pub fn match_command(matches: ArgMatches) {
 I should be able to select my doctype and have everything else be OK. */
 fn generate_config(args: &ArgMatches) -> ProjectConfig {
     let mut config = ProjectConfig::new();
-    let mut input = String::new();
+    let mut input: String;
 
     let name = args.get_one::<String>("NAME").expect("required").to_owned();
     config.set_name(&name);
 
     // Select doctype.
-    print!(
-        "{} [(a)rticle, (b)ook, or (L)etter]: ",
-        "Select template.".blue()
-    );
-    let _ = io::stdout().flush();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => {}
-        Err(_) => {
-            eprintln!("Invalid input; continuing!");
+    loop {
+        input = String::new();
+        print!(
+            "{} [(a)rticle, (b)ook, or (L)etter]: ",
+            "Select template.".blue()
+        );
+        let _ = io::stdout().flush();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {}
+            Err(_) => {
+                eprintln!("Invalid input; continuing!");
+            }
         }
-    }
-    input = input.trim().to_string();
+        input = input.trim().to_string();
 
-    match &input as &str {
-        "A" | "a" => {
-            config.set_doctype(DocumentType::Article);
-            println!("Selecting {}.", "article".blue());
-        }
-        "B" | "b" => {
-            config.set_doctype(DocumentType::Book);
-            println!("Selecting {}", "book".blue());
-        }
-        "L" | "l" => {
-            config.set_doctype(DocumentType::Letter);
-            println!("Selecting {}", "letter".blue());
-        }
-        _ => {
-            println!("Selecting letter."); // Is the default.
-        }
-    }
-
-    input = String::new();
-    // Select driver.
-    print!(
-        "{} [(P)dflatex, (x)elatex, or (l)ualatex]",
-        "Select driver.".blue()
-    );
-    let _ = io::stdout().flush();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => {}
-        Err(_) => {
-            eprintln!("Invalid input; continuing!");
-        }
-    }
-    input = input.trim().to_string();
-
-    match &input as &str {
-        "P" | "p" => {
-            config.set_driver("pdflatex");
-            println!("Selecting {}", "pdflatex".green());
-        }
-        "X" | "x" => {
-            config.set_driver("xelatex");
-            println!("Selecting {}", "xelatex".green());
-        }
-        "L" | "l" => {
-            config.set_driver("lualatex");
-            println!("Selecting {}", "lualatex".green());
-        }
-        _ => {
-            println!("Selecting pdflatex."); // Is the default.
+        match &input as &str {
+            "A" | "a" => {
+                config.set_doctype(DocumentType::Article);
+                println!("Selecting {}.", "article".green());
+                break;
+            }
+            "B" | "b" => {
+                config.set_doctype(DocumentType::Book);
+                println!("Selecting {}", "book".green());
+                break;
+            }
+            "L" | "l" => {
+                config.set_doctype(DocumentType::Letter);
+                println!("Selecting {}", "letter".green());
+                break;
+            }
+            _ => {
+                println!("Selecting {}.", "letter".green()); // Is the default.
+                break;
+            }
         }
     }
 
-    input = String::new();
-    // Choose citations.
-    print!("{} (Y/n) ", "Include citations?".blue());
-    let _ = io::stdout().flush();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => {}
-        Err(_) => {
-            eprintln!("Invalid input; continuing!");
+    loop {
+        input = String::new();
+        // Select driver.
+        print!(
+            "{} [(P)dflatex, (x)elatex, or (l)ualatex]: ",
+            "Select driver.".blue()
+        );
+        let _ = io::stdout().flush();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {}
+            Err(_) => {
+                eprintln!("Invalid input; continuing!");
+            }
         }
-    }
-    input = input.trim().to_string();
+        input = input.trim().to_string();
 
-    match &input as &str {
-        "Y" | "y" => {
-            config.set_citations(true);
-            println!("Setting citations to {}", "true".blue());
-        }
-        "N" | "n" => {
-            config.set_citations(false);
-            println!("Setting citations to {}", "false".red());
-        }
-        _ => {
-            println!("Including citations."); // Is the default.
-        }
-    }
-
-    input = String::new();
-    // Choose graphics.
-    print!("{} (Y/n) ", "Include graphics?".blue());
-    let _ = io::stdout().flush();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => {}
-        Err(_) => {
-            eprintln!("Invalid input; continuing!");
-        }
-    }
-    input = input.trim().to_string();
-
-    match &input as &str {
-        "Y" | "y" => config.set_graphics(true),
-        "N" | "n" => config.set_graphics(false),
-        _ => {
-            println!("Including graphics."); // Is the default.
+        match &input as &str {
+            "P" | "p" => {
+                config.set_driver("pdflatex");
+                println!("Selecting {}", "pdflatex".green());
+                break;
+            }
+            "X" | "x" => {
+                config.set_driver("xelatex");
+                println!("Selecting {}", "xelatex".green());
+                break;
+            }
+            "L" | "l" => {
+                config.set_driver("lualatex");
+                println!("Selecting {}", "lualatex".green());
+                break;
+            }
+            _ => {
+                println!("Selecting pdflatex."); // Is the default.
+                break;
+            }
         }
     }
 
+    loop {
+        input = String::new();
+        // Choose citations.
+        print!("{} (Y/n) ", "Include citations?".blue());
+        let _ = io::stdout().flush();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {}
+            Err(_) => {
+                eprintln!("Invalid input; continuing!");
+            }
+        }
+        input = input.trim().to_string();
+
+        match &input as &str {
+            "Y" | "y" => {
+                config.set_citations(true);
+                println!("Setting citations to {}", "true".green());
+                break;
+            }
+            "N" | "n" => {
+                config.set_citations(false);
+                println!("Setting citations to {}", "false".green());
+                break;
+            }
+            _ => {
+                println!("Setting citations to {}", "true".green()); // Is the default.
+                break;
+            }
+        }
+    }
+
+    loop {
+        input = String::new();
+        // Choose graphics.
+        print!("{} (Y/n) ", "Include graphics?".blue());
+        let _ = io::stdout().flush();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {}
+            Err(_) => {
+                eprintln!("Invalid input; continuing!");
+            }
+        }
+        input = input.trim().to_string();
+
+        match &input as &str {
+            "Y" | "y" => {
+                config.set_graphics(true);
+                println!("Setting graphics to {}", "true".green());
+                break;
+            }
+            "N" | "n" => {
+                config.set_graphics(false);
+                println!("Setting graphics to {}", "false".green());
+                break;
+            }
+            _ => {
+                println!("Setting graphics to {}", "true".green()); // Is the default.
+                break;
+            }
+        }
+    }
     config
 }
