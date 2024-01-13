f56135776da5cfd8a51c241a8456c1a1c7bc5827
Ethan Barry <ethanbarry@howdytx.net>
2024-01-12 20:22:07 -0600

    Using cumaea!

--- src/cli.rs
+++ src/cli.rs
@@ -17,166 +17,100 @@
 // + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
 
 use crate::{config::*, DocumentType};
-use colored::*;
-use std::{io, io::Write};
 
-/// Prompt for yes/no values and return an Option<bool>.
-// TODO Extract to its own crate and publish on crates.io.
-pub fn prompt_tf(prompt: &str, colored: &str, color: &str) -> Option<bool> {
-    let mut input = String::new();
-    input.clear();
-    match color {
-        "blue" => print!(
-            "{}? {} (Y/n): ",
-            prompt.to_owned().trim(),
-            colored.trim().to_owned().blue()
-        ),
-        "red" => print!(
-            "{}? {} (Y/n): ",
-            prompt.to_owned().trim(),
-            colored.trim().to_owned().red()
-        ),
-        "green" => print!(
-            "{}? {} (Y/n): ",
-            prompt.to_owned().trim(),
-            colored.trim().to_owned().green()
-        ),
-        "yellow" => print!(
-            "{}? {} (Y/n): ",
-            prompt.to_owned().trim(),
-            colored.trim().to_owned().yellow()
-        ),
-        _ => print!("{} (Y/n): ", prompt.to_owned().trim()),
-    }
-    let _ = io::stdout().flush();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => {}
-        Err(_) => {
-            eprintln!("Invalid input; continuing!");
-        }
-    }
-    input = input.trim().to_string();
-    match &input as &str {
-        "Y" | "y" => Some(true),
-        "N" | "n" => Some(false),
-        _ => None,
-    }
-}
-
-fn prompt_selection(prompt: &str, colored: &str, color: &str) -> Option<String> {
-    let mut input = String::new();
-    input.clear();
-    match color {
-        "blue" => print!(
-            "{}: [{}]: ",
-            prompt.trim().to_owned(),
-            colored.trim().to_owned().blue()
-        ),
-        "red" => print!(
-            "{}: [{}]: ",
-            prompt.trim().to_owned(),
-            colored.trim().to_owned().red()
-        ),
-        "green" => print!(
-            "{}: [{}]: ",
-            prompt.trim().to_owned(),
-            colored.trim().to_owned().green()
-        ),
-        "yellow" => print!(
-            "{}: [{}]: ",
-            prompt.trim().to_owned(),
-            colored.trim().to_owned().yellow()
-        ),
-        _ => print!(
-            "{}: [{}]: ",
-            prompt.trim().to_owned(),
-            colored.trim().to_owned()
-        ),
-    }
-    let _ = io::stdout().flush();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => {}
-        Err(_) => {
-            eprintln!("Invalid input; continuing!");
-        }
-    }
-    input = input.trim().to_string();
-    Some(input)
-}
-
-// TODO: Restructure menu after adding choices to the templates.
-// | <Select Driver>
-// | <Select Document Category>
-// | <Common> | <Mathematical> | <Formal> | <Personal> | <Technical>
-// TODO: Make graphics and citations dependent on the document type.
-// | <Select Citations>
-// | <Select Graphics>
 pub fn config_menu(name: &str) -> ProjectConfig {
-    let mut config = ProjectConfig::new();
+    use cumaea::{Choice::*, ChoiceColor::*}; // Import enums from the cumaea crate.
 
+    let mut config = ProjectConfig::new();
     config.set_name(name);
 
     // Prompt for driver:
-    match prompt_selection(
+    match cumaea::prompt_selection(
         "Select driver",
-        "(P)dflatex, (x)elatex, (l)ualatex",
-        "green",
+        "(P)dflatex, (l)ualatex, (x)elatex",
+        Some(Normal(Green)),
+        "p",
     )
-    .unwrap_or_default()
+    .to_ascii_lowercase()
     .as_str()
     {
-        "P" | "p" => config.set_driver("pdflatex"),
-        "X" | "x" => config.set_driver("xelatex"),
-        "L" | "l" => config.set_driver("lualatex"),
+        "p" => config.set_driver("pdflatex"),
+        "x" => config.set_driver("xelatex"),
+        "l" => config.set_driver("lualatex"),
         _ => config.set_driver("pdflatex"),
     }
 
-    // Prompt for DocumentType:
-    match prompt_selection(
-        "Select document type",
-        "(A)rticle, (b)ook, (l)etter, (m)athematical article, (n)otes, (p)resentation, (r)ecipe, (t)hesis",
-        "green",
+    match cumaea::prompt_selection(
+        "Select document category",
+        "(M)athematical, (f)ormal, (p)ersonal",
+        Some(Normal(Green)),
+        "m",
     )
-    .unwrap_or_default()
+    .to_ascii_lowercase()
     .as_str()
     {
-        "A" | "a" => config.set_doctype(DocumentType::Article),
-        "B" | "b" => config.set_doctype(DocumentType::Book),
-        "L" | "l" => config.set_doctype(DocumentType::Letter),
-        "M" | "m" => config.set_doctype(DocumentType::MathArticle),
-        "N" | "n" => config.set_doctype(DocumentType::Notes),
-        "P" | "p" => config.set_doctype(DocumentType::Presentation),
-        "R" | "r" => config.set_doctype(DocumentType::Recipe),
-        "T" | "t" => config.set_doctype(DocumentType::Thesis),
-        _ => config.set_doctype(DocumentType::Article),
+        "m" => {
+            match cumaea::prompt_selection(
+                "Select document type",
+                "(N)otes, (b)ook, (m)athematical article, (p)resentation",
+                Some(Normal(Green)),
+                "n",
+            )
+            .to_ascii_lowercase()
+            .as_str()
+            {
+                "b" => config.set_doctype(DocumentType::Book),
+                "m" => config.set_doctype(DocumentType::MathArticle),
+                "n" => config.set_doctype(DocumentType::Notes),
+                "p" => config.set_doctype(DocumentType::Presentation),
+                _ => todo!(),
+            }
+        }
+        "f" => {
+            match cumaea::prompt_selection(
+                "Select document type",
+                "(A)rticle, (t)hesis",
+                Some(Normal(Green)),
+                "a",
+            )
+            .to_ascii_lowercase()
+            .as_str()
+            {
+                "a" => config.set_doctype(DocumentType::Article),
+                "t" => config.set_doctype(DocumentType::Thesis),
+                _ => todo!(),
+            }
+        }
+        "p" => {
+            match cumaea::prompt_selection(
+                "Select document type",
+                "(L)etter, (r)ecipe",
+                Some(Normal(Green)),
+                "l",
+            )
+            .to_ascii_lowercase()
+            .as_str()
+            {
+                "l" => config.set_doctype(DocumentType::Letter),
+                "r" => config.set_doctype(DocumentType::Recipe),
+                _ => todo!(),
+            }
+        }
+        _ => todo!(),
     }
 
     // Prompt for citations:
-    match prompt_tf("Include citations", "", "green") {
-        Some(val) => {
-            if val {
-                config.set_citations(val);
-            } else {
-                config.set_citations(false);
-            }
-        }
-        None => {
-            config.set_citations(true);
-        }
+    if cumaea::prompt_tf_default("Include citations? (Y/n): ", None, true) {
+        config.set_citations(true);
+    } else {
+        config.set_citations(false);
     }
 
     // Prompt for graphics:
-    match prompt_tf("Include graphics", "", "green") {
-        Some(val) => {
-            if val {
-                config.set_graphics(val);
-            } else {
-                config.set_graphics(false);
-            }
-        }
-        None => {
-            config.set_graphics(true);
-        }
+    if cumaea::prompt_tf_default("Include graphics? (Y/n): ", None, true) {
+        config.set_graphics(true);
+    } else {
+        config.set_graphics(false);
     }
 
     config
